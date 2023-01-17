27b4ff2b916a883f00a7a7306aba07e258f221af
andrew1481432 <andrew1481432@mail.ru>
2023-01-17 15:05:18 +0300

    add more test for task 22

--- src/main/java/ru/testerdev/task22/Main.java
+++ src/main/java/ru/testerdev/task22/Main.java
@@ -6,6 +6,37 @@ import ru.testerdev.task22.sort.BundleSort;
 public class Main {
 
     public static void main(String[] args) {
+       testInteger();
+    }
+
+    private static int getRandomDiceNumber() {
+        return (int) (Math.random() * 10) + 1;
+        // Class Random
+    }
+
+    private static void testInteger() {
+        final int SIZE = 10;
+        Integer[] arrInt = new Integer[SIZE];
+        for(int i = 0; i < SIZE; i++) {
+            arrInt[i] = getRandomDiceNumber();
+        }
+
+        System.out.println("DEBUG :: show random elements...");
+        for(Integer el : arrInt) {
+            System.out.print(el+", ");
+        }
+        System.out.println("");
+        BundleSort<Integer> sort = new BundleSort<>();
+        sort.sort(arrInt);
+
+        System.out.println("show sort elements");
+        for(Integer el : arrInt) {
+            System.out.print(el+", ");
+        }
+
+    }
+
+    private static void testWithHouseObj() {
         House[] houses = new House[3];
         houses[0] = new House(100, 120000, "Tokyo", true);
         houses[1] = new House(40, 70000, "Oxford", true);
